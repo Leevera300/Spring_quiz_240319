c80ea91f8dce6405795388d60a55dd3f4f0941fc
DESKTOP-SES5PE0\user <test@test.com>
2024-07-08 18:50:42 +0900

    240708_수업내용

--- src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
+++ src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quiz.lesson07.entity.HiringEntity;
@@ -20,15 +21,48 @@ public class Lesson07Quiz02RestController {
 	@GetMapping("/1")
 	public HiringEntity one() {
 		return hiringrepository.findById(8).orElse(null);
+		// new HiringEntity() == null 인데 이것저것 보여줌
 	}
 	
+	// /lesson07/quiz02/2?companyId=1
 	@GetMapping("/2")
-	public List<HiringEntity> two() {
-		return hiringrepository.findByCompanyId(1);
+	public List<HiringEntity> two(
+			@RequestParam("companyId") int companyId) {
+		
+		return hiringrepository.findByCompanyId(companyId);
 	}
 	
+	// /lesson07/quiz02/3
 	@GetMapping("/3")
 	public List<HiringEntity> three() {
 		return hiringrepository.findByPositionAndType("웹 back-end 개발자", "정규직");
 	}
+	
+	// /lesson07/quiz02/4
+	@GetMapping("/4")
+	public List<HiringEntity> four() {
+		return hiringrepository.findByTypeOrSalaryGreaterThanEqual("정규직", 9000);
+	}
+	
+	// /lesson07/quiz02/5
+	@GetMapping("/5")
+	public List<HiringEntity> five() {
+		return hiringrepository.findTop3ByTypeOrderBySalaryDesc("계약직");
+	}
+	
+	// /lesson07/quiz02/6
+	@GetMapping("/6")
+	public List<HiringEntity> six() {
+		// return hiringrepository.findByRegionAndSalaryGreaterThanEqualAndSalaryLessThanEqual("성남시 분당구", 7000, 8500);
+		return hiringrepository.findByRegionAndSalaryBetween("성남시 분당구", 7000, 8500);
+	}
+	
+	// /lesson07/quiz02/7
+	// 마감일이 2026-04-10 이후이고 연봉이 8100 이상인 정규직 공고를 연봉 내림차순으로 조회하세요.
+	@GetMapping("/7")
+	public List<HiringEntity> seven() {
+		// return hiringrepository.findByTypeAndDeadlineGreaterThanEqualAndSalaryGreaterThanEqualOrderBySalaryDesc("정규직", "2026-04-10", 8100);
+		return hiringrepository.findByDeadlineAndSalaryAndType("2026-04-10", 8100, "정규직");
+	}
+	
 }
--- src/main/java/com/quiz/lesson07/repository/HiringRepository.java
+++ src/main/java/com/quiz/lesson07/repository/HiringRepository.java
@@ -3,11 +3,42 @@ package com.quiz.lesson07.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.quiz.lesson07.entity.HiringEntity;
 
 public interface HiringRepository extends JpaRepository<HiringEntity, Integer>{
 
-	public List<HiringEntity> findByCompanyId(int cId);
+	// JPQL => Entity 조회
+	// quiz02_02
+	public List<HiringEntity> findByCompanyId(int companyId);
+	// quiz02_03
 	public List<HiringEntity> findByPositionAndType(String position, String type);
+	// quiz02_04
+	public List<HiringEntity> findByTypeOrSalaryGreaterThanEqual(String type, int salary);
+	// quiz02_05
+	public List<HiringEntity> findTop3ByTypeOrderBySalaryDesc(String type);
+	// quiz02_06
+	public List<HiringEntity> findByRegionAndSalaryGreaterThanEqualAndSalaryLessThanEqual(String region, int startSalary, int endSalary);
+	public List<HiringEntity> findByRegionAndSalaryBetween(String region, int startSalary, int endSalary);
+	// quiz02_07s
+//	@Query(value = "select * from `hiring` where `type` = :type and `deadline` >= :deadline and `salary` >= :salary Order By `salary` DESC", nativeQuery = true)
+//	public List<HiringEntity> findByTypeAndDeadlineGreaterThanEqualAndSalaryGreaterThanEqualOrderBySalaryDesc(
+//			@Param("type") String type, 
+//			@Param("deadline") String deadline, 
+//			@Param("salary") int salary);
+	// quiz02 7 번 - native query (DB에 직접 SQL)
+	@Query(value = "select * from `hiring` "
+			+ "where `deadline` >= :deadline "
+			+ "and `salary` >= :salary "
+			+ "and `type` = :type "
+			+ "order By `salary` DESC", 
+			nativeQuery = true)
+	public List<HiringEntity> findByDeadlineAndSalaryAndType(
+			@Param("deadline") String deadline, 
+			@Param("salary") int salary,
+			@Param("type") String type);
+	
+	
 }
